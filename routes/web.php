901ad1ca07cb3cf66965a1b07149ff23bebb08fb
Linu <linusundaresan@gmail.com>
2025-02-26 13:15:03 +0530

    user and vendor dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,12 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('frontend.dashboard.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,7 +27,9 @@ Route::post('admin/profile/update' , [ProfileController::class, 'profileUpdate']
 Route::post('admin/password/update' , [ProfileController::class, 'passwordUpdate'])->middleware('auth', 'role:admin')->name('admin.password.update');
 
 
-
+Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'user' , 'as'=>'.user'], function(){
+    Route::get('dashboard' , [UserDashboardController::class, 'index'])->name('dashboard');
+});
 
 Route::get('vendor/dashboard' , [VendorController::class, 'dashboard'])->middleware('auth', 'role:vendor')->name('vendor.dashboard');
 
